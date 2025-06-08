b5afb570d0f85a37627764cf44ddc566ec09219a
Mustafa-Fares <mustaphafares.dev@gmail.com>
2025-06-08 14:16:59 +0300

    Refactor CourseSession model to improve documentation and structure. Added PHPDoc comments for class, properties, and methods, enhancing code readability and maintainability.

--- app/Models/courseSession.php
+++ app/Models/courseSession.php
@@ -2,19 +2,33 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
+use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * Class CourseSession
+ *
+ * Represents a session within a course, including details like title, teacher, and time.
+ */
 class CourseSession extends Model implements HasMedia
 {
-    use HasFactory , InteractsWithMedia;
+    use HasFactory, InteractsWithMedia;
 
-    // Define the table associated with the model
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
     protected $table = 'course_sessions';
 
-    // Define the fillable attributes
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
     protected $fillable = [
         'title',
         'teacher_id',
@@ -27,11 +41,21 @@ class CourseSession extends Model implements HasMedia
         'order'
     ];
 
+    /**
+     * Get the teacher associated with the session.
+     *
+     * @return BelongsTo
+     */
     public function teacher()
     {
         return $this->belongsTo(Teacher::class);
     }
 
+    /**
+     * Get the course associated with the session.
+     *
+     * @return BelongsTo
+     */
     public function course()
     {
         return $this->belongsTo(Course::class);
